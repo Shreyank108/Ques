aab2f59a4c94ab07f0043a09e9f32e5344e717b2
shreyank108 <shreyankagrawal17@gmail.com>
2024-05-12 21:16:12 +0530

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello")
+# Input: 
+grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+# Output: 8 
+# count =0
+# c=""
+# l=[]
+# p=str(grid).replace("[","").replace("]","").replace(" ","")  
+# c+=p                           
+# for i in c.split(",") : 
+#     l.append(int (i)) 
+# for i in l: 
+#     if i<0: 
+#         count +=1  
+# print(count)
+
+count =0
+for i in grid: 
+    for j in i: 
+        if j<0: 
+            count +=1  
+print(count)
