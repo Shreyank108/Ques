402a8f8d578a14c5039871680ee2579e01e6d175
shreyank108 <shreyankagrawal17@gmail.com>
2024-05-13 11:17:05 +0530

    fourth

--- main.py
+++ main.py
@@ -37,17 +37,37 @@ grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
 
 
 # Input: 
-nums = [1,3,4,2,2]
-# Output: 2
-p={}
-for i in nums: 
-    if i in p: 
-        p[i]+=1  
-    else : 
-        p[i] =1   
-print(p)  
+# nums = [1,3,4,2,2]
+# # Output: 2
+# p={}
+# for i in nums: 
+#     if i in p: 
+#         p[i]+=1  
+#     else : 
+#         p[i] =1   
+# print(p)  
 
-for key,value in p.items(): 
-    if value>1: 
-        print(key)
+# for key,value in p.items(): 
+#     if value>1: 
+#         print(key)
     
+    
+# Input: 
+# nums = [8,1,2,2,3]
+# # Output: [4,0,1,1,3] 
+# m=[] 
+# for i in range(len(nums)): 
+#     count =0   
+#     for j in range(len(nums)): 
+#         if nums[i]> nums[j]: 
+#             count +=1  
+#     m.append(count) 
+# print(m) 
+
+
+
+# Input: 
+matrix = [[3,7,8],[9,11,13],[15,16,17]]
+# Output: [15]
+# Explanation: 15 is the only lucky number since it is the minimum in its row and the maximum in its column. 
+
