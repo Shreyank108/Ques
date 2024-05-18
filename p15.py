311520b6b74110309291a663cb37b63ecf34131e
shreyank108 <shreyankagrawal17@gmail.com>
2024-05-18 11:08:33 +0530

    May 18

---
+++ p15.py
@@ -0,0 +1,28 @@
+l=[1,4,2,3,5,6] 
+# l.sort()
+# print(l) 
+# l.sort(reverse=True) 
+# print(l) 
+
+# for i in range(len(l)): 
+#     for j in range(len(l)-1-i): 
+#         if l[i]>l[i+1]: 
+#             p=l[i] 
+#             l[i]=l[i+1] 
+#             l[i+1]=p            
+# print(l) 
+
+# for j in range(len(l)):
+#     for i in range(len(l) - 1 - j):
+#         if l[i] < l[i + 1]:
+#             p = l[i]
+#             l[i] = l[i + 1]
+#             l[i + 1] = p
+# print(l)
+
+l=[1,4,3,2,5,6,7,34,4,3,2] 
+max1 = l[0] 
+for i in range(len(l)): 
+    if l[i]>max1 : 
+        max1=l[i] 
+print(max1)
