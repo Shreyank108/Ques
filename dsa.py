ac45f9847f87e73ba95d1dab3cec327b70c75596
shreyank108 <shreyankagrawal17@gmail.com>
2024-05-18 19:55:29 +0530

    dsa commit may 18

---
+++ dsa.py
@@ -0,0 +1,23 @@
+# firat occurace of number using binary search   
+# def binary(l,n,t): 
+#     s= 0 
+#     e= n  
+#     ans =-1  
+#     while (s<=e): 
+#         mid = (s+e)//2  
+#         if l[mid] == t :  
+#             ans =mid
+#             e=mid-1  
+#         elif l[mid] < t:    
+#             s=mid+1  
+#         else : 
+#             e=mid-1  
+#     return ans 
+
+# l=[1,3,3,3,3,3,4,5]; 
+# n=len(l) 
+# t=3
+# print(binary(l,n,t))
+
+# firat occurace of number using binary search  
+ 
