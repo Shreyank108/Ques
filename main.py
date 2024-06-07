b740e51e4444d682954cb4e13b8d8c75715ec84f
shreyank108 <shreyankagrawal17@gmail.com>
2024-06-07 10:02:59 +0530

    May 7

--- main.py
+++ main.py
@@ -1341,3 +1341,21 @@ s = "string"
 #             diff = max(diff, prefix-x)
 #             prefix = max(prefix, x)
 #         return ans 
+
+
+# Input: 
+n = 10
+m = 3
+# Output: 19
+# Explanation: In the given example:
+# - Integers in the range [1, 10] that are not divisible by 3 are [1,2,4,5,7,8,10], num1 is the sum of those integers = 37.
+# - Integers in the range [1, 10] that are divisible by 3 are [3,6,9], num2 is the sum of those integers = 18.
+# # We return 37 - 18 = 19 as the answer. 
+NotDivisibleByM =0  
+DivisibleByM=0
+for i in range(1,n+1): 
+    if i%m==0: 
+        DivisibleByM+=i   
+    else : 
+        NotDivisibleByM+=i   
+print(NotDivisibleByM-DivisibleByM)
