467c716f3d39552a7bb532bf414ab221934dbda1
shreyank108 <shreyankagrawal17@gmail.com>
2024-06-18 18:33:52 +0530

    18 June

--- main.py
+++ main.py
@@ -1433,11 +1433,31 @@ s = "string"
 #                 return True
 #         return False
 
-x = 18
-count =0
-for i in str(x): 
-        count +=int(i) 
-if x%count==0: 
-        print(True) 
-else : 
-        print(False)
+# x = 18
+# count =0
+# for i in str(x): 
+#         count +=int(i) 
+# if x%count==0: 
+#         print(True) 
+# else : 
+#         print(False)
+
+
+ M = {}
+        maxFreq = 0
+
+        # Iterate through the list and populate the dictionary
+        for num in nums:
+            if num in M:
+                M[num] += 1
+            else:
+                M[num] = 1
+            maxFreq = max(maxFreq, M[num])
+
+        res = 0
+        # Iterate through the dictionary to find elements with frequency equal to maxFreq
+        for key, value in M.items():
+            if value == maxFreq:
+                res += value
+
+        return res
