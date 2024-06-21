5cda947c44f4ef961fa1292a142cf6106c4af866
shreyank108 <shreyankagrawal17@gmail.com>
2024-06-21 09:48:05 +0530

    21 June

--- main.py
+++ main.py
@@ -1490,17 +1490,32 @@ s = "string"
 #         return "none"
 
 
- n = len(nums1)
-        m = len(nums2)
+#  n = len(nums1)
+#         m = len(nums2)
 
-        c1 = 0
-        c2 = 0
+#         c1 = 0
+#         c2 = 0
 
-        for i in range(n) :
-            if nums1[i] in nums2 :
-                c1 += 1
-        for i in range(m) :
-            if nums2[i] in nums1 :
-                c2 += 1
+#         for i in range(n) :
+#             if nums1[i] in nums2 :
+#                 c1 += 1
+#         for i in range(m) :
+#             if nums2[i] in nums1 :
+#                 c2 += 1
                 
-        return [c1, c2]
+#         return [c1, c2]
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    # Merge the customers DataFrame with the orders DataFrame using a left join on 'id' and 'customerId'
+    merged_df = customers.merge(orders, how='left', left_on='id', right_on='customerId')
+    
+    # Use the 'customerId' column to create a boolean mask for customers who never placed any orders
+    mask = merged_df['customerId'].isna()
+    
+    # Filter the rows using the boolean mask
+    result_df = merged_df[mask]
+    
+    # Select only the 'name' column from the result DataFrame and rename it as 'Customers'
+    result_df = result_df[['name']].rename(columns={'name': 'Customers'})
+    
+    return result_df
