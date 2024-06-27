9f3854197739e21c877d38fc91ddb5f6bbab5dee
shreyank108 <shreyankagrawal17@gmail.com>
2024-06-27 18:18:44 +0530

    June 27

--- main.py
+++ main.py
@@ -1581,12 +1581,12 @@ s = "string"
 #   return mappedArray;
 # }; 
 
-var filter = function(arr, fn) {
-    var filteredArr = [];
-    for (var i = 0; i < arr.length; i++) {
-        if (fn(arr[i], i)) {
-            filteredArr.push(arr[i]);
-        }
-    }
-    return filteredArr;
-};
+# var filter = function(arr, fn) {
+#     var filteredArr = [];
+#     for (var i = 0; i < arr.length; i++) {
+#         if (fn(arr[i], i)) {
+#             filteredArr.push(arr[i]);
+#         }
+#     }
+#     return filteredArr;
+# };
