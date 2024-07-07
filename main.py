f14ee95e479eaaa510e0d1108357bc2814aae003
Shreyank108 <shreyankagrawal17@gmail.com>
2024-07-08 01:28:09 +0530

    8 july

--- main.py
+++ main.py
@@ -1594,3 +1594,10 @@ s = "string"
 
 
 # not for today 
+
+return async function(...args) {
+        return new Promise((resolve,reject) => {
+            setTimeout(() => reject("Time Limit Exceeded"),t);
+            fn(...args).then(resolve).catch(reject)
+        })
+    }
