92d94f5c3c8f3c3bd988dd4c723ed357bc412bde
Shreyank108 <shreyankagrawal17@gmail.com>
2024-07-12 19:29:40 +0530

    12 July

--- main.py
+++ main.py
@@ -1595,9 +1595,41 @@ s = "string"
 
 # not for today 
 
-return async function(...args) {
-        return new Promise((resolve,reject) => {
-            setTimeout(() => reject("Time Limit Exceeded"),t);
-            fn(...args).then(resolve).catch(reject)
-        })
-    }
+# return async function(...args) {
+#         return new Promise((resolve,reject) => {
+#             setTimeout(() => reject("Time Limit Exceeded"),t);
+#             fn(...args).then(resolve).catch(reject)
+#         })
+#     } 
+
+
+# Leetcode que no 2
+class Solution:
+    def addTwoNumbers(self, l1: [ListNode], l2: [ListNode]) -> [ListNode]: 
+
+dumyHead=ListNode(0) 
+tail=dumyHead  
+carry=0  
+
+while l1 is not None or l2 is not None or carry !=0 : 
+    digit1 = l1.val if  l1 is not None else 0    
+    digit2 = l2.val if l2 is not None else 0    
+    
+    sum = carry + digit1 + digit2   
+    
+    digit = sum%10   
+    carry = sum//10  
+    
+    newNode = ListNode(digit) 
+    tail.next=newNode   
+    tail=tail.next 
+    
+    l1=l1.next if l1 is not None else None   
+    l2=l2.next if l2 is not None else None   
+    
+result =dumyHead.next   
+dumyHead.next=None   
+return result   
+    
+  
+
