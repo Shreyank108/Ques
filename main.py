1fa9b62f19eca4c19a97218147f14f5b34598cd9
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-08-03 21:28:59 +0530

    3 aug

--- main.py
+++ main.py
@@ -1507,19 +1507,50 @@ s = "string"
 
 
 
-nums = [1,1,0,0,1]
-m = nums.count(1) 
+# nums = [1,1,0,0,1]
+# m = nums.count(1) 
 
-zipped_list = []
-for i in range(len(nums) - m + 1): 
-    zipped_list.append(nums[i:i + m])  
+# zipped_list = []
+# for i in range(len(nums) - m + 1): 
+#     zipped_list.append(nums[i:i + m])  
 
-print(zipped_list)
+# print(zipped_list)
 
-p = [] 
-for i in zipped_list:
-    p.append(i.count(0))  
+# p = [] 
+# for i in zipped_list:
+#     p.append(i.count(0))  
 
-print(min(p))
+# print(min(p))
     
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:  
+        dummy = ListNode()  # Create a dummy node
+        current = dummy     # Set current to the dummy node
+        
+        # Traverse both lists
+        while list1 and list2: 
+            if list1.val < list2.val: 
+                current.next = list1  # Link current node to list1
+                list1 = list1.next     # Move to the next node in list1
+            else: 
+                current.next = list2  # Link current node to list2
+                list2 = list2.next     # Move to the next node in list2
+            
+            current = current.next  # Move current to the last node in the merged list
+
+        # If there are remaining nodes in either list, append them
+        if list1: 
+            current.next = list1   
+        elif list2: 
+            current.next = list2   
+
+        return dummy.next
   
