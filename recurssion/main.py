b7a5a1377295361cc05e7adc79818376f12851e8
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-08-28 12:11:05 +0530

    28 Aug

--- recurssion/main.py
+++ recurssion/main.py
@@ -25,10 +25,92 @@
 #     i+=1  
 # print(count) 
 
-words = ["abab","ab"]
-p=words[0] 
-count=0
-for i in words: 
-    if p in words: 
-        count+=1  
-print(count) 
+# words = ["abab","ab"]
+# p=words[0] 
+# count=0
+# for i in words: 
+#     if p in words: 
+#         count+=1  
+# print(count)  
+
+# l=[7,2,5,3,1] 
+# max =l[0] 
+# second_max =l[0]
+# for i in l: 
+#     if i>max: 
+#         second_max=max  
+#         max = i  
+        
+#     if second_max > max : 
+#             second_max= max
+# print(max)
+# print(second_max) 
+
+# finding second max element 
+# l = [7, 2, 5, 3, 1]
+# max_value = l[0]
+# second_max = l[0]
+
+# for i in l:
+#     if i > max_value:
+#         second_max = max_value  
+#         max_value = i  
+#     elif i > second_max :
+#         second_max = i  
+
+# print("Max:", max_value)
+# print("Second Max:", second_max)
+
+# l = [7, 2, 5, 3, 1]
+
+
+
+#......?
+#second approch  
+# l.sort(reverse=True) 
+# print(l[1]) 
+
+# l=[3,5,4,2,6,7,8] 
+# max_value= float("_inf")
+# Secondmax_value= float("_inf") 
+
+# for i in l: 
+#     if i > max_value : 
+#         Secondmax_value = max_value 
+#         max_value =i  
+#     elif i > Secondmax_value and i !=max_value : 
+#         Secondmax_value= i 
+
+
+# l=[2,1,5,6,3]
+# n=6
+# count =0   
+# for i in l: 
+#     count +=i              
+# p= n*(n+1)/2  
+# print(int(p-count))       
+
+# s1= "shreyank" 
+# s2="hrenkyas" 
+# p=sorted(s1)
+# p1=sorted(s2) 
+# if p==p1: 
+#     print("Anagrams") 
+# else : 
+#     print("not Anagram")  
+
+# n=5   
+# op =12345  
+
+# n=4  
+# op=1234   
+
+
+
+
+
+# multiply two number  
+
+# divide two number   
+
+# 6
