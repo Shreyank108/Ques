15e88659258f48509b5cb68342d9d9a01d8bb6d7
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-09-27 01:22:41 +0530

    27 Sept

---
+++ pattern.py
@@ -0,0 +1,35 @@
+# for i in range (1,5+1): 
+#     # i=1                    
+#     for j in range(i): 
+#         print(j,end=" ") 
+#     print() 
+
+# for i in range(6): 
+#     for j in range(i): 
+#         print('X',end=" ") 
+#     print() 
+
+# for i in range(6): 
+#     for j in range(6-i): 
+#         print('X',end=" ") 
+#     print() 
+
+# for i in range(6,0,-1): 
+#     for j in range(i): 
+#         print('X',end=" ") 
+#     print()
+
+
+for i in range(6): 
+    for j in range(6-i): 
+        print(' ',end="")  
+    for k in range(i): 
+        print("X",end=" ")
+    print()   
+for i in range(6,0,-1): 
+    for j in range(6-i): 
+        print(' ',end="") 
+    for k in range(i): 
+        print("X",end=" ") 
+    print()
+ 
