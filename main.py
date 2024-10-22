45c043012d129f5b59eb80cc01061dcbd1c61c99
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-10-22 23:04:55 +0530

    22 Oct

--- main.py
+++ main.py
@@ -2022,14 +2022,40 @@ s = "((()"
 #         return longest
         
         
-# Input: 
-nums = [3,3]
-target = 6
-# Output: [0,1] 
-l=[]
-for i in range(len(nums)): 
-        for j in range(i+1,len(nums)): 
-                if nums[i]+nums[j] ==target: 
-                        l.append(i) 
-                        l.append(j) 
-print(l)
+# # Input: 
+# nums = [3,3]
+# target = 6
+# # Output: [0,1] 
+# l=[]
+# for i in range(len(nums)): 
+#         for j in range(i+1,len(nums)): 
+#                 if nums[i]+nums[j] ==target: 
+#                         l.append(i) 
+#                         l.append(j) 
+# print(l)
+
+# class Solution:
+#     def isValid(self, word: str) -> bool:
+#         al = 'abcdefghijklmnopqrstuvwxyz'
+#         l, u = False, False
+#         c, v = False, False
+#         d = False
+#         for i in word:
+#             if i.isupper():
+#                 if i.lower() in 'aeiou':
+#                     v = True
+#                 elif i.lower() in al:
+#                     c = True
+#                 u = True
+#             elif i.islower():
+#                 if i in 'aeiou':
+#                     v = True
+#                 elif i in al:
+#                     c = True
+#                 l = True
+#             elif i.isdigit():
+#                 d = True
+#             else:
+#                 return False
+#         an = int(u) + int(l) + int(d)
+#         return len(word) >= 3 and an > 0 and c and v
